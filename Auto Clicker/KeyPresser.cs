018433ed2cf3716d8de4e70a056605b9c9ef3eaa
Ben Thorp <ben.r.thorp@gmail.com>
2020-03-29 16:09:48 -0400

    added auto typer

---
+++ Auto Clicker/KeyPresser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoClicker
+{
+    public class KeyPresser
+    {
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        const int VK_UP = 0x26; //up key
+        const int VK_DOWN = 0x28;  //down key
+        const int VK_LEFT = 0x25;
+        const int VK_RIGHT = 0x27;
+        const uint KEYEVENTF_KEYUP = 0x0002;
+        const uint KEYEVENTF_EXTENDEDKEY = 0x0001;
+        public const uint ONE = 0x31;
+        public const uint TWO = 0x32;
+        public const uint THREE = 0x33;
+        public const uint FOUR = 0x34;
+        public const uint FIVE = 0x35;
+        public const uint SIX = 0x36;
+
+        public static void PressKey(uint key)
+        {
+            //Press the key
+            keybd_event((byte)key, 0, KEYEVENTF_EXTENDEDKEY | 0, 0);
+            keybd_event((byte)key, 0, KEYEVENTF_KEYUP | 0, 0);
+        }
+    }
+}
--- Auto Clicker/MainWindow.xaml.cs
+++ Auto Clicker/MainWindow.xaml.cs
@@ -22,41 +22,41 @@ namespace AutoClicker
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer _dispatcherTimer;
+        DispatcherTimer _autoClickTimer;
+        DispatcherTimer _autoTypeTimer;
         GlobalKeyboardHook _keyboardHooks;
+
+        private uint[] _keysToPress = new uint[] { KeyPresser.ONE, KeyPresser.TWO, KeyPresser.THREE, KeyPresser.FOUR, KeyPresser.FIVE, KeyPresser.SIX };
+        private int _nextKeyIndex = 0;
 
         public MainWindow()
         {
             InitializeKeyLogger();
-            InitializeTimer();
+            InitializeClickTimer();
+            InitializeKeyTimer();
             InitializeComponent();
         }
 
-        private void InitializeKeyLogger()
+        #region AutoClicker
+        private void InitializeClickTimer()
         {
-            _keyboardHooks = new GlobalKeyboardHook(null);
-            _keyboardHooks.KeyboardPressed += OnKeyPressed;
-        }
-
-        private void InitializeTimer()
-        {
-            if (_dispatcherTimer != null)
+            if (_autoClickTimer != null)
             {
-                _dispatcherTimer.Stop();
+                _autoClickTimer.Stop();
             }
             else
             {
-                _dispatcherTimer = new DispatcherTimer();
-                _dispatcherTimer.Tick += TimerTick;
+                _autoClickTimer = new DispatcherTimer();
+                _autoClickTimer.Tick += AutoClickTimerTick;
             }
         }
 
-        private void TimerTick(object sender, EventArgs e)
+        private void AutoClickTimerTick(object sender, EventArgs e)
         {
             ClickTheMouse();
         }
 
-         private void rbnOff_Checked(object sender, RoutedEventArgs e)
+        private void rbnOff_Checked(object sender, RoutedEventArgs e)
         {
             if (rbnOff.IsChecked == true)
             {
@@ -78,7 +78,7 @@ namespace AutoClicker
 
             if (int.TryParse(txtInterval.Text, out intervalValue_ms))
             {
-                SetTimerInterval(intervalValue_ms);
+                SetAutoclickTimerInterval(intervalValue_ms);
             }
         }
 
@@ -90,25 +90,118 @@ namespace AutoClicker
 
         private void TurnOnAutoclicker()
         {
-            if (!_dispatcherTimer.IsEnabled)
+            if (!_autoClickTimer.IsEnabled)
             {
-                _dispatcherTimer.Start();
+                _autoClickTimer.Start();
             }
         }
 
         private void TurnOffAutoclicker()
         {
-            _dispatcherTimer.Stop();
+            _autoClickTimer.Stop();
         }
 
-         private void SetTimerInterval(int intervalValue_ms)
+        private void SetAutoclickTimerInterval(int intervalValue_ms)
         {
-            _dispatcherTimer.Interval = TimeSpan.FromMilliseconds(intervalValue_ms);
+            _autoClickTimer.Interval = TimeSpan.FromMilliseconds(intervalValue_ms);
+        }
+        #endregion
+
+        #region AutoTyper
+        private void InitializeKeyTimer()
+        {
+            if (_autoTypeTimer != null)
+            {
+                _autoTypeTimer.Stop();
+            }
+            else
+            {
+                _autoTypeTimer = new DispatcherTimer();
+                _autoTypeTimer.Tick += AutoKeyTimerTick;
+            }
+        }
+
+        private void AutoKeyTimerTick(object sender, EventArgs e)
+        {
+            PressNextNumberKey();
+        }
+
+        private void PressNextNumberKey()
+        {
+            KeyPresser.PressKey(_keysToPress[_nextKeyIndex]);
+
+            if (_nextKeyIndex >= _keysToPress.Length - 1)
+            {
+                _nextKeyIndex = 0;
+            }
+            else
+            {
+                _nextKeyIndex++;
+            }
+        }
+
+        private void rbnAutotypeOn_Checked(object sender, RoutedEventArgs e)
+        {
+            if (rbnAutotypeOn.IsChecked == true)
+            {
+                TurnOnAutotypeTimer();
+            }
+        }
+
+        private void rbnAutotypeOff_Checked(object sender, RoutedEventArgs e)
+        {
+            if (rbnAutotypeOff.IsChecked == true)
+            {
+                TurnOffAutotypeTimer();
+            }
+        }
+
+        private void txtAutotypeInterval_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int intervalValue_ms;
+
+            if (int.TryParse(txtAutotypeInterval.Text, out intervalValue_ms))
+            {
+                SetAutotypeTimerInterval(intervalValue_ms);
+            }
+        }
+
+        private void TurnOnAutotypeTimer()
+        {
+            if (!_autoTypeTimer.IsEnabled)
+            {
+                _autoTypeTimer.Start();
+            }
+        }
+
+        private void TurnOffAutotypeTimer()
+        {
+            _autoTypeTimer.Stop();
+        }
+        
+        private void SetAutotypeTimerInterval(int intervalValue_ms)
+        {
+            _autoTypeTimer.Interval = TimeSpan.FromMilliseconds(intervalValue_ms);
+        }
+
+        #endregion
+
+        #region KeyLogger
+        private void InitializeKeyLogger()
+        {
+            _keyboardHooks = new GlobalKeyboardHook(null);
+            _keyboardHooks.KeyboardPressed += OnKeyPressed;
         }
 
         private void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
         {
             HandleAutoClickerShortcut(e);
+            HandleAutoTyperShortcut(e);
+
+            //if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyUp)
+            //{
+            //    txtKeypresses.Text = e.KeyboardData.Key.ToString();
+            //}
         }
 
         private void HandleAutoClickerShortcut(GlobalKeyboardHookEventArgs e)
@@ -116,9 +209,24 @@ namespace AutoClicker
             if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyUp &&
                 e.KeyboardData.Key == Keys.F6)
             {
+                e.Handled = true;
                 rbnOn.IsChecked = !rbnOn.IsChecked;
                 rbnOff.IsChecked = !rbnOn.IsChecked; //make sure the off rbn is in the right state
             }
         }
+
+        private void HandleAutoTyperShortcut(GlobalKeyboardHookEventArgs e)
+        {
+            if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyUp &&
+                e.KeyboardData.Key == Keys.F5)
+            {
+                e.Handled = true;
+                rbnAutotypeOn.IsChecked = !rbnAutotypeOn.IsChecked;
+                rbnAutotypeOff.IsChecked = !rbnAutotypeOn.IsChecked; //make sure the off rbn is in the right state
+            }
+        }
+
+        #endregion
+
     }
 }
